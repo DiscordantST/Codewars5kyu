aabb21c95b62b722bfbef534ee9f42bceeafadf9
DiscordantST <discordantstas@gmail.com>
2022-11-29 21:59:28 +0300

    #solution ValidParentheses.py

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,3 +1,7 @@
 def valid_parentheses(string):
-    #your code here
-    return False
+    cnt = 0
+    for char in string:
+        if char == '(': cnt += 1
+        if char == ')': cnt -= 1
+        if cnt < 0: return False
+    return True if cnt == 0 else False
