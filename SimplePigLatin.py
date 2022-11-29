99c9b2d15b0f8e7eecf30a5912334cb10ccc2577
DiscordantST <discordantstas@gmail.com>
2022-11-29 19:44:01 +0300

    #my very bad solution SimplePigLatin.py

--- SimplePigLatin.py
+++ SimplePigLatin.py
@@ -1,2 +1,12 @@
 def pig_it(text):
-    pass
+    simple_list = text.split()
+    suffix = 'ay'
+    simple_list_reverse = []
+    for i in simple_list:
+        for j in range(len(i)):
+            if j == 0:
+                if i[j].isalpha():
+                    simple_list_reverse.append(i[j + 1:] + i[j] + suffix + " ")
+                else:
+                    simple_list_reverse.append(i[j])
+    return ("".join(simple_list_reverse).strip())
