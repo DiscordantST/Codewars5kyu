49705d69882ef0e2e004b3ac87634cf0b7a95ad2
DiscordantST <discordantstas@gmail.com>
2022-12-03 14:55:26 +0300

    #refactoring CalculatingWithFunctions.py

---
+++ CalculatingWithFunctions.py
@@ -0,0 +1,29 @@
+def zero(): #your code here
+    pass
+def one(): #your code here
+    pass
+def two(): #your code here
+    pass
+def three(): #your code here
+    pass
+def four(): #your code here
+    pass
+def five(): #your code here
+    return 5
+def six(): #your code here
+    pass
+def seven(): #your code here
+    return 7
+def eight(): #your code here
+    pass
+def nine(): #your code here
+    pass
+
+def plus(): #your code here
+    pass
+def minus(): #your code here
+    pass
+def times(): #your code here
+    pass
+def divided_by(): #your code here
+    pass
