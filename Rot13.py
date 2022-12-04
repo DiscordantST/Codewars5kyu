63aec0c5e5552ba27cb2239a9707e564b58906e1
DiscordantST <discordantstas@gmail.com>
2022-12-04 19:58:51 +0300

    #solution Rot13.py

--- Rot13.py
+++ Rot13.py
@@ -1,2 +1,19 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet_uper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
 def rot13(message):
-    pass
+    simple_string_decod_rot13 = ""
+    d = 0
+    for i in message:
+        if i in alphabet:
+            d += alphabet.index(i) - 13
+            simple_string_decod_rot13 += alphabet[d]
+            d = 0
+        elif i in alphabet_uper:
+            d += alphabet_uper.index(i) - 13
+            simple_string_decod_rot13 += alphabet_uper[d]
+            d = 0
+        else:
+            simple_string_decod_rot13 += i
+    return simple_string_decod_rot13
