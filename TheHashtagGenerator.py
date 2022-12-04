fe408eac0ff48d11cf64457c955f9f9a31c21ec7
DiscordantST <discordantstas@gmail.com>
2022-12-04 20:15:10 +0300

    #refactoring solution TheHashtagGenerator.py

--- TheHashtagGenerator.py
+++ TheHashtagGenerator.py
@@ -2,5 +2,5 @@ def generate_hashtag(s):
     if len(s) > 140 or len(s) == 0:
         return False
     else:
-        hashtagstring = "#" + s.title().replace(' ', '')
-        return hashtagstring
+        hashtagString = "#" + s.title().replace(' ', '')
+        return hashtagString
