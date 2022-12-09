c12d290075446575c60e8441428ffe75b778e702
DiscordantST <discordantstas@gmail.com>
2022-12-09 20:48:30 +0300

    #not my solution AdditiveNumbers.py

--- AdditiveNumbers.py
+++ AdditiveNumbers.py
@@ -1,10 +1,15 @@
 def find_additive_numbers(num):
+    for k in range(2, len(num)):
+        for j in range(1, k):
+            if k-j>1 and num[j] == '0': continue
+            a, b = j, k
+            out = [num[:a], num[a:b]]
+            while b < len(num):
+                x = int(out[-2]) + int(out[-1])
+                a, b = b, b + len(str(x))
+                y = int(num[a:b])
+                if x != y: break
+                out.append(str(y))
+            else:
+                return out
     return []
-
-
-num = '1992358'
-c = []
-for i in num:
-    c.append(i)
-
-print(c)
